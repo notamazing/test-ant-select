ae9ebd3d8f4e60ea381c9e27276432f8816b0e7f
Grace DuMez <grace.dumez@atomicobject.com>
2020-01-20 13:58:47 -0500

    update index

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import 'regenerator-runtime/runtime';
 
 const content = (
   <div>
-    <p>Phone: (800) 555-4321</p>
-    <p>Email: info@fmic.com</p>
+    <p>Phone: (800) 888-8888</p>
+    <p>Email: email@email.com</p>
   </div>
 );
 
@@ -16,17 +16,9 @@ export default () => {
   const {Option} = Select;
   return (
   <div style={{height: "500px", width:"500px", paddingTop:"50px", paddingLeft:"50px", backgroundColor: "red"}}>
-    {/* <Form name="blah">
-      <Form.Item name="input" rules={[{type: "number"}]}>
-        <Input/>
-      </Form.Item>
-      <Form.Item>
-        <Button htmlType="submit">Submit</Button>
-      </Form.Item>
-    </Form> */}
     <Row>
 
-    <Select defaultValue="lucy" style={{ width: 120 }}  getPopupContainer={(e) => e.parentElement}>
+    <Select defaultValue="lucy" style={{ width: 120 }}>
       <Option value="jack">Jack</Option>
       <Option value="lucy">Lucy</Option>
       <Option value="disabled" disabled>
@@ -40,7 +32,6 @@ export default () => {
           title={title}
           content={content}
           trigger="click"
-          getPopupContainer={(e) => e.parentElement}
         >
           <Button>HELP</Button>
         </Popover>
